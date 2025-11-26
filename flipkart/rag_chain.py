7776a12ae0c73aec78ff38b2209dcb76f6c81038
Solomon Apenya <solawd@gmail.com>
2025-11-26 11:33:31 +0000

    Make corrections to lanchain imports

--- flipkart/rag_chain.py
+++ flipkart/rag_chain.py
@@ -1,5 +1,6 @@
 from langchain_groq import ChatGroq
-from langchain_community.chains import create_history_aware_retriever,create_retrieval_chain
+from langchain_classic.chains.history_aware_retriever import create_history_aware_retriever
+from langchain_classic.chains.retrieval import create_retrieval_chain
 from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
 from langchain_core.runnables.history import RunnableWithMessageHistory
